16446bbc5421744382d5bfa44fbaaddaf8498816
Allan Wirth <allan@allanwirth.com>
2025-01-26 10:54:00 +0900

    Add prefix option

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This repo contains a bash script (`maven2git.sh`) that will create a git reposit
 
 To use it, you must have `gsutil` installed because it pulls from the [Google GCS Maven Central Mirror](https://storage-download.googleapis.com/maven-central/index.html).
 
-Usage is: `./maven2git.sh <groupId>:<artifactId>`.
+Usage is: `./maven2git.sh <groupId>:<artifactId>`. Multiple arguments can be specified.
+
+The script takes one argument --prefix (or -p) which takes an argument that will be interpreted as a prefix and parse all the artifacts that match that prefix. For example, `--prefix com.google` will process all artifacts in `com.google` or any group that starts with `com.google`.
 
 The cache of downloaded packages will be created in `./cache` and the output repos will be stored in `./repos`.
--- maven2git.sh
+++ maven2git.sh
@@ -4,12 +4,32 @@ set -euo pipefail
 
 shopt -u dotglob # do not include hidden files in globs
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <groupId:artifactId>"
-  exit 1
+# getopt parsing (any number of positional arguments, or --prefix which takes one argument)
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --prefix)
+            PREFIX="$2"
+            shift
+            shift
+            ;;
+        *)
+            POSITIONAL+=("$1")
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$PREFIX" ] && [ ${#POSITIONAL[@]} -eq 0 ]; then
+    echo "Usage: $0 [--prefix PREFIX] [TARGET...]"
+    exit 1
 fi
 
-TARGET="$1"
+if [ -n "$PREFIX" ] && [ ${#POSITIONAL[@]} -gt 0 ]; then
+    echo "Error: Cannot specify both --prefix and TARGET"
+    exit 1
+fi
 
 required_tools=(git gsutil unzip)
 for tool in "${required_tools[@]}"; do
@@ -21,43 +41,57 @@ done
 
 cache_dir="./cache"
 repos_dir="./repos"
-mirror="gs://maven-central/maven2/"
+mirror="gs://maven-central/maven2"
 
-target_dir=$(echo $TARGET | tr ':.' '/')
-artifactid=$(echo $TARGET | cut -d: -f2)
 
-mkdir -p "$cache_dir/$target_dir"
-
-gsutil -m rsync -r "$mirror$target_dir" "$cache_dir/$target_dir"
-
-# Note: versions are already sorted in maven-metadata.xml
-versions=$(grep -oP "<version>.*</version>" "$cache_dir/$target_dir/maven-metadata.xml" | sed -e 's/<version>\(.*\)<\/version>/\1/')
-
-out_repo="$repos_dir/$target_dir"
-if [ -d "$out_repo" ]; then
-    echo "Error: Repository already exists at $out_repo"
-    exit 1
+if [ -n "$PREFIX" ]; then
+    prefix_dir=$(echo $PREFIX | tr ':.' '/')
+    target_dirs=$(gsutil ls -r "$mirror/$prefix_dir" |  grep -E 'maven-metadata\.xml$' | sed -E 's$^gs://maven-central/maven2/(.*)/maven-metadata.xml$\1$')
+else
+    target_dirs=()
+    for target in "${POSITIONAL[@]}"; do
+        target_dir=$(echo $target | tr ':.' '/')
+        target_dirs+=("$target_dir")
+    done
 fi
-mkdir -p "$out_repo"
-git init "$out_repo"
 
-for version in $versions; do
-    echo "Processing $artifactid:$version"
 
-    rm -fr "$out_repo/"*
-    
-    base_path="$cache_dir/$target_dir/$version/$artifactid-$version"
-    cp -r "$base_path.pom" "$out_repo/pom.xml"
-    if [ -f "$base_path-sources.jar" ]; then
-        mkdir -p "$out_repo/sources"
-        unzip -q "$base_path-sources.jar" -d "$out_repo/sources"
+for target_dir in "${target_dirs[@]}"; do
+    artifactid=$(basename $target_dir)
+
+    mkdir -p "$cache_dir/$target_dir"
+
+    gsutil -m rsync -r "$mirror/$target_dir" "$cache_dir/$target_dir"
+
+    # Note: versions are already sorted in maven-metadata.xml
+    versions=$(grep -oP "<version>.*</version>" "$cache_dir/$target_dir/maven-metadata.xml" | sed -e 's/<version>\(.*\)<\/version>/\1/')
+
+    out_repo="$repos_dir/$target_dir"
+    if [ -d "$out_repo" ]; then
+        echo "Error: Repository already exists at $out_repo"
+        exit 1
     fi
-    if [ -f "$base_path-javadoc.jar" ]; then
-        mkdir -p "$out_repo/javadoc"
-        unzip -q "$base_path-javadoc.jar" -d "$out_repo/javadoc"
-    fi
-    git -C "$out_repo" add .
-    git -C "$out_repo" commit -q -m "Add $artifactid-$version"
+    mkdir -p "$out_repo"
+    git init "$out_repo"
+
+    for version in $versions; do
+        echo "Processing $artifactid:$version"
+
+        rm -fr "$out_repo/"*
+        
+        base_path="$cache_dir/$target_dir/$version/$artifactid-$version"
+        cp -r "$base_path.pom" "$out_repo/pom.xml"
+        if [ -f "$base_path-sources.jar" ]; then
+            mkdir -p "$out_repo/sources"
+            unzip -q "$base_path-sources.jar" -d "$out_repo/sources"
+        fi
+        if [ -f "$base_path-javadoc.jar" ]; then
+            mkdir -p "$out_repo/javadoc"
+            unzip -q "$base_path-javadoc.jar" -d "$out_repo/javadoc"
+        fi
+        git -C "$out_repo" add .
+        git -C "$out_repo" commit -q -m "Add $artifactid-$version"
+    done
+
+    echo "Done. Repository is at $out_repo"
 done
-
-echo "Done. Repository is at $out_repo"
